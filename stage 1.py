8c73b39b5505e4016d73a671930324125369a6e7
Lee <tkdduq2346@gmail.com>
2022-08-26 00:08:16 +0900

    stage1~5 complete

--- stage 1.py
+++ stage 1.py
@@ -1,1 +1,18 @@
-print("안녕하세요~")
+#1번
+print("Hello World")
+
+#2번
+for i in range(2):
+    print("강한친구 대한육군")
+
+#3번
+A, B=map(int, input().split())
+print(A+B)
+
+#4번
+A, B=map(int, input().split())
+print(A-B)
+
+#5번
+A, B=map(int, input().split())
+print(A*B)
